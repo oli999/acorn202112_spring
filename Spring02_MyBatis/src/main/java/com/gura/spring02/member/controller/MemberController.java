541c2974ffe597227d1700e8ad9deff6d6cb7c93
kimgura <oli999@naver.com>
2022-03-03 12:28:40 +0900

    Spring02 프로젝트에 /member/list2.do 요청 처리함

--- Spring02_MyBatis/src/main/java/com/gura/spring02/member/controller/MemberController.java
+++ Spring02_MyBatis/src/main/java/com/gura/spring02/member/controller/MemberController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.gura.spring02.member.dao.MemberDao;
 import com.gura.spring02.member.dto.MemberDto;
@@ -26,6 +27,18 @@ public class MemberController {
 		//3. view page 로 forward 이동해서 회원목록을 응답한다. 
 		
 		return "member/list"; // /WEB-INF/views/member/list.jsp 
+	}
+	
+	@RequestMapping("/member/list2")
+	public ModelAndView list2(ModelAndView mView) {
+		//1. dao 를 이용해서 DB 에서 회원 목록을 얻어온다.
+		List<MemberDto> list=dao.getList();
+		//2. 회원 목록을 ModelAndView 객체에 담는다.
+		mView.addObject("list", list);
+		//3. view page 의 정보를 ModelAndView 객체에 담는다.
+		mView.setViewName("member/list");
+		//4. ModelAndView 객체를 리턴해 준다.
+		return mView;
 	}
 }
 
