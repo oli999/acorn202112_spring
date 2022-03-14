1109362cd5a701db3d1ae61567d30d3d5139c062
kimgura <oli999@naver.com>
2022-03-14 12:15:13 +0900

    Spring03 에 카페 글 수정 기능 구현함

--- Spring03_Final/src/main/java/com/gura/spring03/cafe/controller/CafeController.java
+++ Spring03_Final/src/main/java/com/gura/spring03/cafe/controller/CafeController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -97,6 +98,19 @@ public class CafeController {
 
 		return "redirect:/cafe/list.do";
 	}
+	@RequestMapping("/cafe/private/updateform")
+	public String updateForm(HttpServletRequest request) {
+
+		service.getData(request);
+
+		return "cafe/updateform";
+	}
+	//카페글 수정 요청 처리 
+	@RequestMapping(value = "/cafe/private/update", method = RequestMethod.POST)
+	public String update(CafeDto dto) {
+		service.updateContent(dto);
+		return "cafe/update";
+	}	
 }
 
 
