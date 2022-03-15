55552959cffaab73c86186d71654c97a036e40e7
kimgura <oli999@naver.com>
2022-03-15 10:10:58 +0900

    Spring04_Aop 프로젝트 추가함

---
+++ Spring04_Aop/src/test/main/MainClass.java
@@ -0,0 +1,12 @@
+package test.main;
+
+import test.util.WritingUtil;
+
+public class MainClass {
+	public static void main(String[] args) {
+		WritingUtil util=new WritingUtil();
+		util.write1();
+		util.write2();
+		util.write3();
+	}
+}
