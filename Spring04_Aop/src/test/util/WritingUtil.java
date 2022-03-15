b85c07af18109b883db1d3875f7f6cb00c7aee6b
kimgura <oli999@naver.com>
2022-03-15 10:46:36 +0900

    Spring04 예제에 AOP 적용함

--- Spring04_Aop/src/test/util/WritingUtil.java
+++ Spring04_Aop/src/test/util/WritingUtil.java
@@ -6,18 +6,12 @@ import org.springframework.stereotype.Component;
 public class WritingUtil {
 
 	public void write1() {
-		System.out.println("파란색 Pen 을 준비해요");
 		System.out.println("편지를 써요");	
-		System.out.println("Pen 을 마무리 해요");
 	}
 	public void write2() {
-		System.out.println("파란색 Pen 을 준비해요");
 		System.out.println("보고서를 써요");
-		System.out.println("Pen 을 마무리 해요");
 	}
 	public void write3() {
-		System.out.println("파란색 Pen 을 준비해요");
 		System.out.println("일기를 써요");
-		System.out.println("Pen 을 마무리 해요");
 	}
 }
